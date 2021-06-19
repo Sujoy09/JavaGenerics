944ca6065a6662da34f27c5fa6db4a320b852aef
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-06-19 11:46:30 +0530

    ADDED: PrintMyArray Sample Code

---
+++ PrintMyArray.java
@@ -0,0 +1,27 @@
+package com.bridgelabz.DummyTesting;
+
+public class PrintMyArray {
+	public static void main( String[] args ) {
+	int[] a = {1,2,3};
+	double[] b = {1.2, 1.3, 1.4};
+	char[] c = {'a', 'b', 'c'};
+	toPrint(a);
+	toPrint(b);
+	toPrint(c);
+	}
+	private static void toPrint(int[] a) {
+		for (int i:a) {
+			System.out.println(i);
+		}
+	}
+	private static void toPrint(double[] b) {
+		for (double i:b) {
+			System.out.println(i);
+		}
+	}
+	private static void toPrint(char[] c) {
+		for (char i:c) {
+			System.out.println(i);
+		}
+	}
+}

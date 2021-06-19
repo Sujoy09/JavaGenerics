fb1d1c43be52087480d2559f9a1c6c57322873dd
Sujoyroy10 <sujoyroy.619@gmail.com>
2021-06-19 13:27:09 +0530

    ADDED: Generics Class to PrintMyArray Sample Code

--- PrintMyArray.java
+++ PrintMyArray.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.DummyTesting;
 
-public class PrintMyArray {
+public class PrintMyArray<E> {
+	E[] myArray;
+	
+	public PrintMyArray(E[] myArray) {
+		this.myArray = myArray;
+	}
 	public static void main( String[] args ) {
 	Integer[] a = {1,2,3};
 	Double[] b = {1.2, 1.3, 1.4};
@@ -8,10 +13,16 @@ public class PrintMyArray {
 	toPrint(a);
 	toPrint(b);
 	toPrint(c);
+	new PrintMyArray<Integer>(a).toPrint();
+	new PrintMyArray<Double>(b).toPrint();
+	new PrintMyArray<Character>(c).toPrint();
 	}
 	private static <E> void toPrint(E[] a) {
 		for (E i:a) {
 			System.out.println(i);
 		}
+	}
+	private void toPrint() {
+		toPrint(myArray);
 	}
 }
